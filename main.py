bc135386d64254640a1c66c3b7a53770a1579c50
Joker_vD <joker.vd@gmail.com>
2021-08-27 21:53:31 +0300

    Dumping some test lambdas to the stdout

--- main.py
+++ main.py
@@ -3,8 +3,22 @@
 from utils import dummy
 
 
+# good enough for now
+def lam2str(term):
+    return str(term)
+
+def do_work(term, ctx):
+    print(f'{ctx}: {lam2str(term)}')
+
+    print()
+
+
 def main():
-    print(dummy())
+    for i, term in enumerate([
+        ('LAM', 'x', 'x'),
+        ('APP', ('LAM', 'x', 'x'), ('LAM', 'x', 'x')),
+    ]):
+        do_work(term, i)
 
 
 if __name__ == '__main__':
